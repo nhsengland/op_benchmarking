6fe3f0cce4c94ae43c0cce00320e5e89200ac033
johndcusack <john.cusack@nhs.net>
2023-11-01 16:34:55 +0000

    V1_1_0
    Changed the tables so that they treated the columns as HTML number formats in order to prevent NA cells causing the columns to use string sorting. Also tweaked the metric page to a more even layout

--- Rscripts/knit_dashboard.R
+++ Rscripts/knit_dashboard.R
@@ -8,7 +8,7 @@ source('Rscripts\\local_functions.R')
 source('Rscripts\\local_colours.R')
 
 
-rmarkdown::render(input = 'parent_main_v3.Rmd',
+rmarkdown::render(input = 'parent_main_v4.Rmd',
                   output_file = 'SE_OP_Benchmarking_Tool_2023_10_24.html',
                   output_dir = 'outputs')
 
